b4b0658068a4eaacd3a6ebf3788136c8566d2a65
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-02-23 19:28:04 +0500

    icon

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'TUG"ILGAN', 'kUNING BILAN', ' OPA! 🥳🥳🥳😎' ],
+			strings: [ 'TUG"ILGAN', 'kUNING BILAN', ' OPA!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
